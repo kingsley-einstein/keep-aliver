618f24c873378e956b500554c02f2e1390e3312e
Kingsley Victor <javaprodigy56@gmail.com>
2026-02-07 15:59:13 +0100

    chore: add dex api

--- jobs.js
+++ jobs.js
@@ -2,12 +2,13 @@ const scheduler = require("node-cron");
 const axios = require("axios");
 
 const DESIGN_URL = "https://magnetar-finance-designs.onrender.com";
+const DEX_API = "https://magnetar-dex-service.onrender.com/api";
 
 export function keepAlive() {
   scheduler
     .schedule("* * * * *", async () => {
       try {
-        const promises = [axios.get(DESIGN_URL)];
+        const promises = [axios.get(DESIGN_URL), axios.get(DEX_API)];
         await Promise.all(promises);
       } catch (error) {
         console.error(error.stack);
